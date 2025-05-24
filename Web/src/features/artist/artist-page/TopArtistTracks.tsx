0036a808d03dfedd5ed90282943c79253f1bc4ea
Mattias Andersson <mattias800@gmail.com>
2025-05-24 19:09:12 +0200

    Add "Show more" button to display additional tracks
    Updated the TopArtistTracks component to show a subset of tracks by default and added a "Show more" button to reveal more tracks. This improves usability by initially limiting the list size and allowing users to expand it if needed.

--- Web/src/features/artist/artist-page/TopArtistTracks.tsx
+++ Web/src/features/artist/artist-page/TopArtistTracks.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
+import { useState } from "react";
 import { TopArtistTrackItem } from "@/features/artist/artist-page/TopArtistTrackItem.tsx";
 import { FragmentType, graphql, useFragment } from "@/gql";
 import { SectionHeading } from "@/components/headings/SectionHeading.tsx";
+import { SecondaryButton } from "@/components/buttons/SecondaryButton.tsx";
 
 export interface TopArtistTracksProps {
   artist: FragmentType<typeof topArtistTracksArtistFragment>;
@@ -20,14 +22,26 @@ export const topArtistTracksArtistFragment = graphql(`
 export const TopArtistTracks: React.FC<TopArtistTracksProps> = (props) => {
   const artist = useFragment(topArtistTracksArtistFragment, props.artist);
 
+  const [showingMore, setShowingMore] = useState(false);
+
+  const visibleTracks = artist.topTracks.slice(0, showingMore ? 20 : 10);
+
   return (
     <div className="px-6 md:px-10 mt-4">
       <SectionHeading>Popular</SectionHeading>
       <div>
-        {artist.topTracks.map((track, index) => (
+        {visibleTracks.map((track, index) => (
           <TopArtistTrackItem key={track.id} index={index + 1} track={track} />
         ))}
       </div>
+      {!showingMore && (
+        <div className={"pl-14 pt-8"}>
+          <SecondaryButton
+            label={"Show more"}
+            onClick={() => setShowingMore(true)}
+          />
+        </div>
+      )}
     </div>
   );
 };
