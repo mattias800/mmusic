2bc6b8c3870de3a9ded6e4886fd0cce184b56482
Mattias Andersson <mattias800@gmail.com>
2025-08-05 23:21:49 +0200

    - Extract TrackItem UI.

---
+++ Web/src/components/track-item/TrackItem.tsx
@@ -0,0 +1,126 @@
+import * as React from "react";
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuSeparator,
+  ContextMenuSub,
+  ContextMenuSubContent,
+  ContextMenuSubTrigger,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu.tsx";
+import { PlusCircle } from "lucide-react";
+import {
+  formatLargeNumber,
+  formatTrackLength,
+} from "@/common/TrackLengthFormatter.ts";
+import { playlists } from "@/components/playlists.ts";
+import { ReactNode } from "react";
+
+export interface TrackItemProps {
+  coverArtUri?: string | null | undefined;
+  title: string;
+  onClick?: () => void;
+  trackNumber: number;
+  playing?: boolean;
+  playCount: number;
+  trackLength: number;
+  contextMenuItems?: ReactNode | ReactNode[];
+}
+
+export const TrackItem: React.FC<TrackItemProps> = ({
+  coverArtUri,
+  title,
+  onClick,
+  trackNumber,
+  playing,
+  playCount,
+  trackLength,
+  contextMenuItems,
+}) => {
+  return (
+    <ContextMenu>
+      <ContextMenuTrigger>
+        <div
+          className={`grid grid-cols-[40px_80px_1fr_150px_50px] items-center px-4 py-2 rounded hover:bg-neutral-800 ${
+            playing ? "text-green-400 font-semibold" : "text-white"
+          }`}
+        >
+          <span>{playing ? "▶" : trackNumber}</span>
+
+          {coverArtUri ? (
+            <img
+              src={coverArtUri}
+              alt={title ?? ""}
+              className={
+                "h-12 w-12 object-cover transition-all hover:scale-105 aspect-square rounded-md"
+              }
+            />
+          ) : (
+            <div className={"h-12 w-12"} />
+          )}
+
+          <div className={"flex gap-4 items-center"}>
+            {onClick ? (
+              <button
+                className={
+                  "truncate cursor-pointer hover:underline flex items-center gap-4"
+                }
+                onClick={onClick}
+              >
+                <span>{title}</span>
+              </button>
+            ) : (
+              <span>{title}</span>
+            )}
+          </div>
+
+          <span className="text-sm text-neutral-400 text-right">
+            {formatLargeNumber(playCount)}
+          </span>
+          <span className="text-sm text-neutral-400 text-right">
+            {formatTrackLength(trackLength)}
+          </span>
+        </div>
+      </ContextMenuTrigger>
+      <ContextMenuContent className="w-40">
+        <ContextMenuItem>Add to Library</ContextMenuItem>
+        <ContextMenuSub>
+          {contextMenuItems}
+          <ContextMenuSubTrigger>Add to Playlist</ContextMenuSubTrigger>
+          <ContextMenuSubContent className="w-48">
+            <ContextMenuItem>
+              <PlusCircle className="mr-2 h-4 w-4" />
+              New Playlist
+            </ContextMenuItem>
+            <ContextMenuSeparator />
+            {playlists.map((playlist) => (
+              <ContextMenuItem key={playlist}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  className="mr-2 h-4 w-4"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M21 15V6M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM12 12H3M16 6H3M12 18H3" />
+                </svg>
+                {playlist}
+              </ContextMenuItem>
+            ))}
+          </ContextMenuSubContent>
+        </ContextMenuSub>
+        <ContextMenuSeparator />
+        <ContextMenuItem>Play Next</ContextMenuItem>
+        <ContextMenuItem>Play Later</ContextMenuItem>
+        <ContextMenuItem>Create Station</ContextMenuItem>
+        <ContextMenuSeparator />
+        <ContextMenuItem>Like</ContextMenuItem>
+        <ContextMenuItem>Share</ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
+  );
+};
--- Web/src/features/artist/artist-page/TopArtistTrackItem.tsx
+++ Web/src/features/artist/artist-page/TopArtistTrackItem.tsx
@@ -1,23 +1,8 @@
 import { FragmentType, graphql, useFragment } from "@/gql";
 import * as React from "react";
-import {
-  formatLargeNumber,
-  formatTrackLength,
-} from "@/common/TrackLengthFormatter.ts";
-import {
-  ContextMenu,
-  ContextMenuContent,
-  ContextMenuItem,
-  ContextMenuSeparator,
-  ContextMenuSub,
-  ContextMenuSubContent,
-  ContextMenuSubTrigger,
-  ContextMenuTrigger,
-} from "@/components/ui/context-menu.tsx";
-import { Play, PlusCircle, Search } from "lucide-react";
-import { playlists } from "@/components/playlists.ts";
+import { ContextMenuItem } from "@/components/ui/context-menu.tsx";
 import { useNavigate } from "react-router";
-import { RecordingPlayButton } from "@/features/music-players/RecordingPlayButton.tsx";
+import { TrackItem } from "@/components/track-item/TrackItem.tsx";
 
 interface TopArtistTrackItemProps {
   track: FragmentType<typeof topArtistTrackItemLastFmTrackFragment>;
@@ -55,111 +40,27 @@ export const TopArtistTrackItem: React.FC<TopArtistTrackItemProps> = (
   const trackName = track.recording?.title ?? track.name;
 
   return (
-    <ContextMenu>
-      <ContextMenuTrigger>
-        <div
-          className={`grid grid-cols-[40px_80px_1fr_150px_50px] items-center px-4 py-2 rounded hover:bg-neutral-800 ${
-            props.active ? "text-green-400 font-semibold" : "text-white"
-          }`}
-        >
-          <span>{props.active ? "▶" : props.index}</span>
-
-          {track.recording?.mainAlbum?.coverArtUri ? (
-            <img
-              src={track.recording?.mainAlbum?.coverArtUri}
-              alt={track.recording?.mainAlbum?.title ?? ""}
-              className={
-                "h-12 w-12 object-cover transition-all hover:scale-105 aspect-square rounded-md"
+    <TrackItem
+      trackNumber={props.index}
+      title={trackName}
+      trackLength={track.recording?.length ?? 0}
+      playCount={track.playCount}
+      playing={props.active}
+      coverArtUri={track.recording?.mainAlbum?.coverArtUri}
+      contextMenuItems={
+        <>
+          {track.recording?.mainAlbum && (
+            <ContextMenuItem
+              onClick={() =>
+                track.recording?.mainAlbum &&
+                navigate(`/album/${track.recording.mainAlbum.id}`)
               }
-            />
-          ) : (
-            <div className={"h-12 w-12"} />
-          )}
-
-          <div className={"flex gap-4 items-center"}>
-            {track.recording ? (
-              <RecordingPlayButton
-                recording={track.recording}
-                renderButton={(onClick, needsYoutubeSearch) => (
-                  <button
-                    className={
-                      "truncate cursor-pointer hover:underline flex items-center gap-4"
-                    }
-                    onClick={onClick}
-                  >
-                    <span>{trackName}</span>
-                    {needsYoutubeSearch ? (
-                      <Search className={"h-4 w-4"} />
-                    ) : (
-                      <Play className={"h-4 w-4"} />
-                    )}
-                  </button>
-                )}
-                renderWhenNotPlayable={() => (
-                  <span className="truncate">{trackName}</span>
-                )}
-              />
-            ) : (
-              <span className="truncate text-red-900">{trackName}</span>
-            )}
-          </div>
-
-          <span className="text-sm text-neutral-400 text-right">
-            {formatLargeNumber(track.playCount)}
-          </span>
-          <span className="text-sm text-neutral-400 text-right">
-            {formatTrackLength(track.recording?.length ?? 0)}
-          </span>
-        </div>
-      </ContextMenuTrigger>
-      <ContextMenuContent className="w-40">
-        {track.recording?.mainAlbum && (
-          <ContextMenuItem
-            onClick={() =>
-              track.recording?.mainAlbum &&
-              navigate(`/album/${track.recording.mainAlbum.id}`)
-            }
-          >
-            Go to album
-          </ContextMenuItem>
-        )}
-
-        <ContextMenuItem>Add to Library</ContextMenuItem>
-        <ContextMenuSub>
-          <ContextMenuSubTrigger>Add to Playlist</ContextMenuSubTrigger>
-          <ContextMenuSubContent className="w-48">
-            <ContextMenuItem>
-              <PlusCircle className="mr-2 h-4 w-4" />
-              New Playlist
+            >
+              Go to album
             </ContextMenuItem>
-            <ContextMenuSeparator />
-            {playlists.map((playlist) => (
-              <ContextMenuItem key={playlist}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  className="mr-2 h-4 w-4"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M21 15V6M18.5 18a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5ZM12 12H3M16 6H3M12 18H3" />
-                </svg>
-                {playlist}
-              </ContextMenuItem>
-            ))}
-          </ContextMenuSubContent>
-        </ContextMenuSub>
-        <ContextMenuSeparator />
-        <ContextMenuItem>Play Next</ContextMenuItem>
-        <ContextMenuItem>Play Later</ContextMenuItem>
-        <ContextMenuItem>Create Station</ContextMenuItem>
-        <ContextMenuSeparator />
-        <ContextMenuItem>Like</ContextMenuItem>
-        <ContextMenuItem>Share</ContextMenuItem>
-      </ContextMenuContent>
-    </ContextMenu>
+          )}
+        </>
+      }
+    />
   );
 };
