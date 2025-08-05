bf54e5e219c7f187ec24da619acc0e6b56e7cb1f
Mattias Andersson <mattias800@gmail.com>
2025-08-06 01:03:19 +0200

    - Add id to FileSystemEntry.

--- MusicGQL/Features/FileSystem/FileSystemEntry.cs
+++ MusicGQL/Features/FileSystem/FileSystemEntry.cs
@@ -1,3 +1,13 @@
 namespace MusicGQL.Features.FileSystem;
 
-public record FileSystemEntry(string Name, string Path, bool IsDirectory, bool HasChildren, bool IsAccessible);
+public record FileSystemEntry(
+    string Name,
+    string Path,
+    bool IsDirectory,
+    bool HasChildren,
+    bool IsAccessible
+)
+{
+    [ID]
+    public string Id() => Path;
+}
